f7d57ca062fbaaa2130a4aeea9f8ecffffe6d2ad
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-05-03 08:54:22 -0500

    Can access webpages.

--- app/src/main/java/com/ctech/max/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ctech/max/photogallery/PhotoGalleryFragment.java
@@ -1,14 +1,20 @@
 package com.ctech.max.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 public class PhotoGalleryFragment extends Fragment {
 
@@ -22,6 +28,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -35,5 +42,18 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrlString("https://raytf.org");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
